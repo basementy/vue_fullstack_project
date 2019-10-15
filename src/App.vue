5e3bfbcbcea304bea6faf0777412a4def1154a61
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2019-10-14 22:15:52 -0300

    Init the projects

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    // All the used components
+  }
+}
+</script>
+
+<style>
+</style>
